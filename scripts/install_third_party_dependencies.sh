ad20061e9c9cbd10e83013785f667b445b6c4602
pujaltes <seby131@gmail.com>
2024-04-03 20:49:43 +0100

    Changes for installing on hpc

--- scripts/install_third_party_dependencies.sh
+++ scripts/install_third_party_dependencies.sh
@@ -20,5 +20,5 @@ conda env config vars set CUTLASS_PATH=$PWD/cutlass
 # This setting is used to fix a worker assignment issue during data loading
 conda env config vars set KMP_AFFINITY=none
 
-export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+#export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
+#export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
